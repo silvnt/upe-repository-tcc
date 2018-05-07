cb6bda3d9d31390ba596e1a282f40503c82135d0
Matheus-felipe <matheus0906.mhci@gmail.com>
2018-05-07 11:21:20 -0300

    au

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
- /* eslint-disable no-console */
-
- //console.log('UPE Repository TCC Project Bootstrap...');
+/* eslint-disable no-console */
 
 
- /*Testing persistence*/ 
-const conn = require('persistence/ConnectionDB.js');
-const TccPersis = require('persistence/TCCPersistence.js'); 
+/* Testing persistence */
+const conn = require('./persistence/ConnectionDB.js');
+const TccPersis = require('./persistence/TCCPersistence.js');
 
 const tccRep = new TccPersis(conn);
 
-tccRep.insert('1234', 'Putaria', 'sexo', 'Teteu_picudo', 'Sistemas de Prostituição', '2018', '2').then((res) => {
+tccRep.insert('1234', 'Putaria', 'sexo', 'Teteu_picudo', 'Sistemas de Prostituição', '2018', '2').then(() => {
   console.log('Deu certo!');
 }).catch((res) => {
   console.log(res);
