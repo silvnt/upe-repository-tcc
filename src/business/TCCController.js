b8766cf3efd4871921bb668df289fb81b863a13f
Matheus-felipe <matheus0906.mhci@gmail.com>
2018-05-14 13:59:25 -0300

    Persistence fix

--- src/business/TCCController.js
+++ src/business/TCCController.js
@@ -6,7 +6,6 @@ class TCCController {
   insertTcc(isbn, titulo, tema, autor, curso, ano, semestre) {
     return new Promise((resolve, reject) => {
       this.tccPersistor.insert(isbn, titulo, tema, autor, curso, ano, semestre).then((res) => {
-        console.log('Inseriu!!!');
         resolve(res);
       }).catch((res) => {
         reject(res);
