16e389f68f476764286dbfaf7c39d67c6f013c50
Matheus-felipe <matheus0906.mhci@gmail.com>
2018-06-10 19:15:55 -0300

    Delete some comments

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
 /* eslint-disable no-console */
 
-
-/* Testing persistence */
-/*
-const conn = require('./persistence/ConnectionDB.js');
-const TccPersis = require('./persistence/TCCPersistence.js');
-
-const tccRep = new TccPersis(conn);
-
-tccRep.insert(
-  '1236', 'Puta',
-  'AUAUAUAU', 'teteu', 'Sistemas de Informação', '2018', '3',
-).then(() => {
-  console.log('Deu certo!');
-}).catch((res) => {
-  console.log(res);
-});
-*/
-
-
 const routesTCC = require('./routes/TccRoutes.js');
 
 const e = require('express');
